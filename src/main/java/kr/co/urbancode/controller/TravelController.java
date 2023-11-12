7235fa6ffb4f4f5589f753f1a4d62119a60c9d45
DESKTOP-LFNS18E\urbancode <sysysy0302@gmail.com>
2023-11-12 21:28:38 +0900

    commit

--- src/main/java/kr/co/urbancode/controller/TravelController.java
+++ src/main/java/kr/co/urbancode/controller/TravelController.java
@@ -1,33 +1,57 @@
 package kr.co.urbancode.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import kr.co.urbancode.dto.PlaceDTO;
 import kr.co.urbancode.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //컨트롤러 -> 서비스 호출
 
-@EnableSwagger2
 @RestController
 @RequiredArgsConstructor //(롬복)초기화되지 않은 final/@NonNull 필드 생성자 생성 후 의존성 주입
 @Slf4j //로깅
 @RequestMapping("/travel") //하위 메서드 모두에 공통 url 설정
-//@Tag(name="travel", description = "여행지 API")
+@Api(tags = {"여행지 API"})
 public class TravelController {
 	
 	private final PlaceService placeService;
 
+	@ApiOperation(value="contentId를 이용하여 여행지 레코드를 조회합니다")
 	@GetMapping(value = "/{contentId}")
-//	@Operation(summary = "여행지 조회",description = "contentId를 이용해 여행지를 조회합니다")
-	public PlaceDTO getOnePlace(@PathVariable int contentId) {
-		return 	placeService.getOnePlace(contentId);
+	public ResponseEntity<PlaceDTO> getOnePlace(@ApiParam(value = "여행지 id", defaultValue = "125266") @PathVariable int contentId) {
+		return 	ResponseEntity.ok(placeService.getOnePlace(contentId));
     }
 
+	@ApiOperation(value="contentId를 이용하여 여행지 레코드를 삭제합니다")
+	@DeleteMapping(value="/{contentId}")
+	public void deletePlace(@ApiParam(value = "여행지 id", defaultValue = "125266") @PathVariable int contentId){
+		placeService.deletePlace(contentId);
+	}
+
+	@ApiOperation(value="여행지 레코드를 수정합니다")
+	@PutMapping(value="/put")
+	public void updatePlace(@ApiParam(value="여행 DTO") @RequestBody PlaceDTO placeDTO){
+		placeService.updatePlace(placeDTO);
+	}
+
+	@ApiOperation(value="여행지 레코드를 추가합니다")
+	@PostMapping(value="/post")
+	public void insertPlace(@ApiParam(value="여행 DTO") @RequestBody PlaceDTO placeDTO){
+		placeService.insertPlace(placeDTO);
+	}
+
+	@ApiOperation(value="여행지 레코드 전체를 조회합니다")
+	@GetMapping(value="/list")
+	public ResponseEntity<List<PlaceDTO>> getPlaceList(){
+		return ResponseEntity.ok(placeService.getPlaceList());
+	}
 	/*
 	@GetMapping(value = "/entity/{contentId}")
 	public ResponseEntity<PlaceDTO> getREPlace(@PathVariable int contentId) {
--- src/main/java/kr/co/urbancode/mapper/PlaceMapper.java
+++ src/main/java/kr/co/urbancode/mapper/PlaceMapper.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 //mapper -> sqlSession(sql 쿼리 실행)
 
 @Repository
@@ -14,7 +16,7 @@ public class PlaceMapper {
    //해당 빈이 생성자를 통해 주입되는 시점에 불변성 보장하도록 final 키워드 추가
    private final SqlSession sqlSession;
 
-   public PlaceDTO getOnePlace(int contentId){
+   public PlaceDTO selectOnePlace(int contentId){
       //mapper.xml namespace에서 풀클래스명 사용시 쿼리문 id만 사용해도 됨
       //return sqlSession.selectOne("selectOnePlace", contentId);
 
@@ -22,9 +24,24 @@ public class PlaceMapper {
       return sqlSession.selectOne("mapper.PlaceMapper.selectOnePlace", contentId);
    }
 
+   public void deletePlace(int contentId){
+       sqlSession.delete("mapper.PlaceMapper.deletePlace", contentId);
+   }
+
+   public void updatePlace(PlaceDTO placeDTO){
+      sqlSession.update("mapper.PlaceMapper.updatePlace", placeDTO);
+   }
+
+   public void insertPlace(PlaceDTO placeDTO) {
+      sqlSession.insert("mapper.PlaceMapper.insertPlace", placeDTO);
+   }
+
+   public List<PlaceDTO> selectPlaceList() {
+      return sqlSession.selectList("mapper.PlaceMapper.selectPlaceList");
+   }
 }
 
-/**
+/*
  * @RequiredArgsConstructor
  *  : final/@NonNull 키워드가 붙은 필드를 파라미터로 갖는 생성자를 만들고 주입해준다. (아래 코드와 같은 의미)
  *  : (주의) 생성자의 파라미터 순서는 클래스 내부에 선언된 필드의 순서와 동일해야 한다
